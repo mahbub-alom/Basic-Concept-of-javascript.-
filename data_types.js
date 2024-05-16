c05b57fa23d56fc2794ca24e61dd2059377fc805
Md Mahbub Alom <mdmahbuba034@gmail.com>
2024-05-16 22:48:32 +0600

    data_types concept clear

--- data_types.js
+++ data_types.js
@@ -3,16 +3,9 @@
  * Primitive: String,boolean,number,null,undefined.
  * Non-Primitive:Reference Variable(Array,object,Function).
  *
+ * ---------------------------------------------------
+ * Mutability: Primitive data types are immutable, while non-primitive types are mutable.
+ * Storage: Primitive data types are stored directly in memory, whereas non-primitive   types are stored by reference.
  */
 
 let a = 5;
-let b = a;
-b = 6;
-// console.log("a:",a)
-// console.log("b:",b)
-
-let num1 = [1, 2, 3, 4];
-let num2 = [num1];
-num2.push(5);
-console.log("num1:", num1);
-console.log("num2:", num2);
