d826cde036aa9133237178f0ab92250f677d5936
Md Mahbub Alom <114336626+mahbub-alom@users.noreply.github.com>
2024-05-17 08:55:58 +0600

    Update data_types.js

--- data_types.js
+++ data_types.js
@@ -9,3 +9,13 @@
  */
 
 let a = 5;
+let b = a;
+b = 6;
+// console.log("a:", a);
+// console.log("b:", b);
+
+const num1 = [1, 2, 3, 4];
+const num2 = num1;
+num2.push(5)
+console.log("num1:", num1);
+console.log("num2:", num2);
